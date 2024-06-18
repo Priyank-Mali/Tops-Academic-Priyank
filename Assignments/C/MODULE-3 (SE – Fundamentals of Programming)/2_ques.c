f0ecade46fdf7d4751d77737ad0194f067481a0d
priyank <priyankmali297@gmail.com>
2024-06-18 20:04:41 +0530

    C

---
+++ Assignments/C/MODULE-3 (SE – Fundamentals of Programming)/2_ques.c
@@ -0,0 +1,8 @@
+// Write a program to make Simple calculator (to make addition, subtraction, multiplication, division and modulo) ?
+
+#include<stdio.h>
+
+int main(){
+    float num1,num2,result;
+
+}
