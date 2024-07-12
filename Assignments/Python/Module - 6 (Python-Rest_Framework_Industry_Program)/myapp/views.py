ca0d354a67752f0d59b6c21ff1ae2560c112c073
priyank <priyankmali297@gmail.com>
2024-07-12 14:00:16 +0530

    assignment

--- Assignments/Python/Module - 6 (Python-Rest_Framework_Industry_Program)/myapp/views.py
+++ Assignments/Python/Module - 6 (Python-Rest_Framework_Industry_Program)/myapp/views.py
@@ -11,14 +11,24 @@ def book_list(request):
     if request.method=='GET':
         bookqueryset = Book.objects.all()
         serializer = BookSerializer(bookqueryset,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response({
+            "status" : status.HTTP_200_OK,
+            "payload" : serializer.data
+        })
     
     elif request.method=='POST':
         serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response({
+                "status" : status.HTTP_201_CREATED,
+                "payload" : serializer.data,
+                "message" : "Data Created Successfully"
+            })
+        return Response({
+            "status" : status.HTTP_400_BAD_REQUEST,
+            "error" : serializer.errors
+        })
 
 
 @api_view(['GET','PUT','PATCH','DELETE'])
@@ -26,28 +36,51 @@ def book_details(request,pk):
     try:
         bookqueryset = Book.objects.get(pk=pk)
     except Book.DoesNotExist:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response({
+            "status" : status.HTTP_400_BAD_REQUEST,
+            "mesage" : "Data not Exists"
+        })
     
     if request.method=='GET':
         serializer = BookSerializer(bookqueryset)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response({
+            "status" : status.HTTP_200_OK,
+            "payload" : serializer.data
+        })
     
     #  The PUT method is used to update a resource completely. It requires the entire object to be updated.
     elif request.method=='PUT':
         serializer = BookSerializer(bookqueryset,data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response({
+                "status" : status.HTTP_201_CREATED,
+                "payload" : serializer.data,
+                "message" : "Data Updated Successfully"
+            })
+        return Response({
+            "status" : status.HTTP_400_BAD_REQUEST,
+            "error" : serializer.errors
+        })
     
     # The PATCH method is used to update a resource partially. It allows you to send only the fields that need to be updated.
     elif request.method=='PATCH':
         serializer = BookSerializer(bookqueryset,data=request.data,partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response({
+                "status" : status.HTTP_201_CREATED,
+                "payload" : serializer.data,
+                "message" : "Data updated partially successfully"
+            })
+        return Response({
+            "status" : status.HTTP_400_BAD_REQUEST,
+            "error" : serializer.errors
+        })
     
     elif request.method=='DELETE':
         bookqueryset.delete()
-        return Response(status=status.HTTP_410_GONE)
+        return Response({
+            "status" : status.HTTP_410_GONE,
+            "message" : "Data deleted Successfully"
+        })
