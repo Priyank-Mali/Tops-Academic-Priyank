5f6769766cf1c255ea36995c894067b4b364d608
priyank <priyankmali297@gmail.com>
2024-08-20 16:25:56 +0530

    assignment

---
+++ Assignments/C/(Module-3.1)-C Language Fundamental/1_ques.c
@@ -0,0 +1,14 @@
+// 1.	Display This Information using printf 
+// a.	Your Name 
+// b.	Your Birth date 
+// c.	Your Age 
+// d.	Your Address 
+
+#include<stdio.h>
+
+int main(){
+    printf("Priyank Mali\n");
+    printf("29-07-1998\n");
+    printf("25\n");
+    printf("Harinagar 2\n");
+}
