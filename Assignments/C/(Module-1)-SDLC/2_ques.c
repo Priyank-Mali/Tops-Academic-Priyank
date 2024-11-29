8d03454e46019c374acf20f75b19788b43d31491
🇵‌🇷‌🇮‌🇾‌🇦‌🇳‌🇰‌ 🇲‌🇦‌🇱‌🇮‌ <priyankmali297@gmail.com>
2024-11-29 20:37:20 +0530

    assignment

--- Assignments/C/(Module-1)-SDLC/2_ques.c
+++
@@ -1,8 +0,0 @@
-// Write a program to make Simple calculator (to make addition, subtraction, multiplication, division and modulo) ?
-
-#include<stdio.h>
-
-int main(){
-    float num1,num2,result;
-
-}
